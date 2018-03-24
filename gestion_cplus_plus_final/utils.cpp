9484431e2e5bd8e02686997599d254a1f98918af
wogno <31076843+wogno@users.noreply.github.com>
2018-03-24 13:18:26 +0000

    Mise a jour de ajouter

--- gestion_cplus_plus_final/utils.cpp
+++ gestion_cplus_plus_final/utils.cpp
@@ -2,18 +2,42 @@
 
 void insererEmploye(Cemploye *tab, int &i)
 {
-
+  string nom_, prenom_;
+  Status status_;
 }
 
 void rechercherEmplpoye(Cemploye *tab, int i)
 {
-  for(int d=0; d<t; 
+  string nom_;
+  cout>>nom_;
+  for(int d=0; d<i; d++)
+    {
+    if (tab[d].getNom() == nom_)
+      {
+      tab[d].afficher();
+      break;// verifier si l employer est unique 
+    }
+    
+  }
 
 }
 
 void supprimerEmploye(Cemploye *tab, int &i)
 {
+  string matricule_;
+  cout <<"entrer votre numero matricule";
+  for(int d=0; d<i; d++){
+    if(tab[d].getMatricule() == matricule_)
+      {
+        for(int t=d; t<i; t++)
+          {
+            tab[t] = tab[++t]
+          }
+      break;
+      }
+      
 
+}
 }
 
 
@@ -50,7 +74,7 @@ void fonctionnaire(Cemploye *tab, int i)
       nb++;
     }
     }
-  cout<<" Le rapport des fonctionnaire est de : "<<endl;
+  cout<<" Le rapport des fonctionnaire est de : "<<(nb/i)*100<<endl;
 
 
 }
