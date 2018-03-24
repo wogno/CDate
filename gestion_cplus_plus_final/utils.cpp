000030639b16fe50b72e6fff584493206c9792e8
wogno <31076843+wogno@users.noreply.github.com>
2018-03-24 09:36:19 +0000

    Mise a jour de afficher et fonctionnaire

--- gestion_cplus_plus_final/utils.cpp
+++ gestion_cplus_plus_final/utils.cpp
@@ -7,6 +7,7 @@ void insererEmploye(Cemploye *tab, int &i)
 
 void rechercherEmplpoye(Cemploye *tab, int i)
 {
+  for(int d=0; d<t; 
 
 }
 
@@ -24,6 +25,13 @@ void listeRetraite(Cemploye *tab, int i)
 
 void masseSalariale(Cemploye *tab, int i)
 {
+  double sum=0;
+ for (int d=0; d<i; d++)
+   {
+      sum+=tab[d].calculerSalaire();
+   }
+  cout<<"La masse salariale: "<<sum<<endl;
+  
 
 }
 
@@ -34,6 +42,16 @@ void calculeSalaireEmp(Cemploye *tab, int i)
 
 void fonctionnaire(Cemploye *tab, int i)
 {
+  int nb=0;
+  for(int d=0; d<i; d++)
+    {
+    if(!tab[d].estAuxiliaire())
+      {
+      nb++;
+    }
+    }
+  cout<<" Le rapport des fonctionnaire est de : "<<endl;
+
 
 }
 
